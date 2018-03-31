3a0bfc803e840fda7735d28c94214f2ce6d4458e
HoangThiKimPhung <hoangkimphung070994@gmail.com>
2018-03-31 21:16:00 +0700

    first

---
+++ QuanLyTTTinHoc_SpringBoot/src/main/java/com/example/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.demo.DemoApplication;
+
+@SpringBootApplication 
+public class ServletInitializer {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		SpringApplication.run(DemoApplication.class, args);
+	} 
+}
